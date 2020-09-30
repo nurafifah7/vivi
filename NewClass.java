b37611a0513ae29749fe0e84ee58107cc5c03886
nurafifah7 <72062870+nurafifah7@users.noreply.github.com>
2020-09-30 15:35:46 +0700

    Add files via upload
    Percobaan 1

--- NewClass.java
+++ NewClass.java
@@ -6,8 +6,21 @@
 
 /**
  *
- * @author user
+ * @author Faa
  */
-public class NewClass {
-    
+public class new class {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 8, y = 13;
+        System.out.println("x =" +x);
+        System.out.println("y =" +y);
+        System.out.println("x =" + ++x);
+        System.out.println("x =" + y++);
+        System.out.println("x =" + x--);
+        
+    }
 }
