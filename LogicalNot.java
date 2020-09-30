50db6c23efb9497d7702dec04041b00fa246c416
nurafifah7 <72062870+nurafifah7@users.noreply.github.com>
2020-09-30 15:26:46 +0700

    Add files via upload
    Percobaan Logical Not

---
+++ LogicalNot.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class LogicalNot {
+    // Java code to illustrate 
+ // logical NOT operator import java.io.*;
+  
+    public static void main(String[] args) 
+    { 
+        // initializing variables 
+        int a = 10, b = 1; 
+  
+        // Displaying a, b, c 
+        System.out.println("Var1 = " + a); 
+        System.out.println("Var2 = " + b); 
+  
+        // Using logical NOT operator 
+        System.out.println("!(a < b) = " + !(a < b)); 
+        System.out.println("!(a > b) = " + !(a > b)); 
+    } 
+} 
