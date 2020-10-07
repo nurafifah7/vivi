2bbcaae99b179116475cb81d0e6d43c6841f949f
nurafifah7 <72062870+nurafifah7@users.noreply.github.com>
2020-10-07 20:01:10 +0700

    Add files via upload

---
+++ SteatmentIf.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class SteatmentIf {
+    public static void main (String[] args){
+    int nilai_ALJABAR = 80;
+      
+    if(nilai_ALJABAR > 70){
+      /*
+       * Jika nilai/value pada variable nilai_MTK lebih dari 70
+       * Maka program akan menjalankan statement ini
+       */   
+      System.out.println("Selamat kamu lulus ujian ALJABAR");
+    }
+  }
+}
