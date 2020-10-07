81713ced37a672cf88991f251423a6313719be13
nurafifah7 <72062870+nurafifah7@users.noreply.github.com>
2020-10-07 20:01:53 +0700

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifelse;
+
+/**
+ *
+ * @author user
+ */
+public class IfElse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int nilai_algoritma = 80;
+      
+    if(nilai_algoritma < 70){
+      /*
+       * Jika nilai/value pada variable nilai_MTK lebih dari 70
+       * Maka program akan menjalankan statement ini
+       */   
+      System.out.println("Selamat vivi kamu lulus ujian algoritma");
+    }else {
+      /*
+       * Dan Jika nilai/value pada variable nilai_MTK kurang dari 70
+       * Maka program akan menjalankan statement ini
+       */  
+      System.out.println("Mohon maaf vivi kamu tidak lulus");
+    }
+    }
+    
+}
